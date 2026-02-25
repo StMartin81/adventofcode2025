81e28e35c58daa3a2fa99144c971e0748cd31ee3
Martin Stolpe <martin.stolpe@gmail.com>
2026-02-25 22:20:18 +0100

    Day 1, solution 2

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,16 +1,18 @@
 use anyhow::{Result, anyhow};
 use std::fs::read_to_string;
 
+#[derive(Debug)]
 pub enum Direction {
     Left,
     Right,
 }
 
 fn main() -> Result<()> {
-    let mut number_of_zeros: u32 = 0;
+    let mut number_of_zeros: i32 = 0;
     let contents = read_to_string("day01/input")?;
-    let mut position: u32 = 50;
-    let max_pos: u32 = 100; // Actually this is max_pos + 1
+    let mut position: i32 = 50;
+    let mut old_position: i32 = 50;
+    let max_pos: i32 = 100; // Actually this is max_pos + 1
     for line in contents.lines() {
         let (direction, clicks) = line.split_at(1);
         let direction = match direction {
@@ -24,22 +26,41 @@ fn main() -> Result<()> {
             }
             _ => Err(anyhow!("Unexpected character.")).unwrap(),
         };
-        let clicks = clicks.parse::<u32>().unwrap() % max_pos;
-        // println!("Number of clicks: {}", clicks);
+        let mut clicks = clicks.parse::<i32>().unwrap();
+        println!(
+            "Number of clicks: {}, direction: {:?}, current position: {}",
+            clicks, direction, position
+        );
+        let overflowing_zeros = clicks / max_pos;
+        if overflowing_zeros != 0 {
+            println!("Overflow zeros: {}", overflowing_zeros);
+        }
+        number_of_zeros = number_of_zeros + overflowing_zeros;
+        clicks = clicks % max_pos;
         match direction {
             Direction::Left => {
-                if position >= clicks {
-                    position = position - clicks
-                } else {
-                    position = max_pos - (clicks - position)
-                }
+                position = position - clicks;
             }
-            Direction::Right => position = (position + clicks) % max_pos,
+            Direction::Right => {
+                position = position + clicks;
+            }
         };
-        // println!("New position: {}", position);
-        if position == 0 {
+        if position >= max_pos {
+            position = position - max_pos;
+            number_of_zeros = number_of_zeros + 1;
+        } else if position < 0 {
+            position = position + max_pos;
+            if old_position != 0 {
+                number_of_zeros = number_of_zeros + 1;
+            }
+        } else if position == 0 {
             number_of_zeros = number_of_zeros + 1;
         }
+        println!(
+            "Number of zeros: {},New position: {}",
+            number_of_zeros, position
+        );
+        old_position = position;
     }
     println!("Number of zeros: {}", number_of_zeros);
     Ok(())
