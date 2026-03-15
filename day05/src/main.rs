0e6caf220a4c227a1e098762b05c1f371d876c48
Martin Stolpe <martin.stolpe@gmail.com>
2026-03-15 17:38:45 +0100

    Day 5, solution 2

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -12,24 +12,17 @@ fn main() -> Result<()> {
     let contents = read_to_string("day05/input").context("Cannot read file")?;
     let contents = contents.trim();
     let mut ranges = VecDeque::<Range>::new();
-    let mut items = VecDeque::<u64>::new();
-    let mut reading_ranges = true;
     for line in contents.lines() {
         if line.is_empty() {
-            reading_ranges = false;
-            continue;
+            break;
         }
 
-        if reading_ranges {
-            let range = line.split('-');
-            let range: Vec<&str> = range.collect();
-            ranges.push_back(Range {
-                begin: range[0].parse()?,
-                end: range[1].parse()?,
-            });
-        } else {
-            items.push_back(line.parse()?);
-        }
+        let range = line.split('-');
+        let range: Vec<&str> = range.collect();
+        ranges.push_back(Range {
+            begin: range[0].parse()?,
+            end: range[1].parse()?,
+        });
     }
     let mut ranges: Vec<&Range> = ranges.iter().collect();
     ranges.sort_by(|a: &&Range, b: &&Range| a.begin.cmp(&b.begin));
@@ -50,29 +43,10 @@ fn main() -> Result<()> {
             consolidated_ranges.push_back(range.clone());
         }
     }
-    let mut fresh_items: u64 = 0;
-    let mut items_sorted: Vec<u64> = items.into_iter().collect();
-    items_sorted.sort();
-    let mut items_queue = VecDeque::new();
-    for item in items_sorted {
-        items_queue.push_back(item);
-    }
-    let consolidated_ranges = consolidated_ranges.into_iter();
+    let mut total_fresh_item_ids: u64 = 0;
     for range in consolidated_ranges {
-        loop {
-            if let Some(item) = items_queue.pop_front() {
-                if item >= range.begin && item <= range.end {
-                    fresh_items = fresh_items + 1;
-                } else if item > range.end {
-                    items_queue.push_front(item);
-                    break;
-                }
-            } else {
-                // no more items
-                break;
-            }
-        }
+        total_fresh_item_ids = total_fresh_item_ids + range.end - range.begin + 1;
     }
-    println!("Number of fresh items: {}", fresh_items);
+    println!("Total number of fresh item IDs: {}", total_fresh_item_ids);
     Ok(())
 }
